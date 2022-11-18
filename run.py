08db2271e78b2a215c8e3d831c7255dc520898f4
Nathaniel Singer <nathaniel@singer.cloud>
2022-11-17 21:53:07 -0800

    app configuration and bug fixes

--- run.py
+++ run.py
@@ -1,65 +1,80 @@
-#!/usr/bin/python3
+# python native imports
+import os
+import sys
+import time
+import requests
+import logging
 
-# library imports
-import os, sys, time, requests, logging, google.cloud.logging
+# third party imports
+import google.cloud.logging
 from twilio.rest import Client
 
-# logging configuration
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="auth.json"
 
-google_log_client = google.cloud.logging.Client()
-google_log_client.setup_logging()
+# environment setup 
+LOG_LOCAL      = os.environ.get("LOG_LOCAL", False) 
+INTERVAL_SEC   = os.environ.get("INTERVAL_SEC", 60)
+ACCOUNT_SID    = os.environ.get("ACCOUNT_SID")
+AUTH_TOKEN     = os.environ.get("AUTH_TOKEN")
+SENDER_PHONE   = os.environ.get("SENDER_PHONE")
+RECEIVER_PHONE = os.environ.get("RECEIVER_PHONE")
+URL_REG        = os.environ.get("URL_REG")
+URL_TEST       = os.environ.get("URL_TEST")
+URL_WARNING    = os.environ.get("URL_WARNING")
 
-logging.basicConfig(stream=sys.stdout,
-    level=logging.DEBUG,
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
+awe_reg_message = "Black hat Advanced Windows \
+Exploitation \registration is online. Visit \
+https://www.blackhat.com/us-23/training/schedule to register now!"
+
+awe_test_message = "This is a test message to \
+validate that twilio is successfully configured. \
+You will receive a message from the bot when course \
+registration becomes available."
+
+awe_warning_message = "The Advanced Windows Exploitation \
+Black Hat notification tool received an unexpected status \
+code from the website. You should check out the tool and \
+website to see if something is broken or if the course is \
+about to go live. The tool will continue to run just in \
+case the course is about to go live."
 
 
-# globals 
-DELAY = 60 # in seconds
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-from_phone_number = os.environ['TWILIO_FROM_PHONE_NUMBER']
-to_phone_number = os.environ['TWILIO_TO_PHONE_NUMBER']
+# logging configuration, local or remote
+if LOG_LOCAL:
+    logging.basicConfig(stream=sys.stdout,
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+else: google.cloud.logging.Client().setup_logging()
 
 
 # twilio client establishment
-client = Client(account_sid, auth_token)
-
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 
 # twilio action wrappers
 def send_text_message(message_content:str) -> str:
     return client.messages.create(
-        body=message_content,
-        from_=from_phone_number,
-        to=to_phone_number
+        to=RECEIVER_PHONE,
+        from_=SENDER_PHONE,
+        body=message_content
     ).sid
-
 
 def make_phone_call(content_url:str) -> str:
     return client.calls.create(
-        url=content_url,
-        from_=from_phone_number,
-        to=to_phone_number
+        to=RECEIVER_PHONE,
+        from_=SENDER_PHONE,
+        url=content_url
     ).sid
-
-
-awe_reg_message = "Black hat Advanced Windows Exploitation registration is online. Visit https://www.blackhat.com/us-23/training/schedule to register now!"
-awe_test_message = "This is a test message to validate that twilio is successfully configured. You will receive a message from the bot when course registration becomes available."
-awe_warning_message = "The Advanced Windows Exploitation Black Hat notification tool received an unexpected status code from the website. You should check out the tool and website to see if something is broken or if the course is about to go live. The tool will continue to run just in case the course is about to go live."
-
-awe_reg_url = "https://handler.twilio.com/twiml/EH923e23e88b7be9d5cac065342bf323e7"
-awe_test_url = "https://handler.twilio.com/twiml/EHae0b918431d9218ab8e1d13dedbf8aae"
-awe_warning_url = "https://handler.twilio.com/twiml/EHca803ad458b7437ca271794fd0534660"
 
 
 # send test cases to verify tool is successfully configured
 logging.info(f"sending test cases to validate that the tool is successfully configured")
 logging.info(f"executing a text message to user, status code '{send_text_message(awe_test_message)}'")
-logging.info(f"executing a phone call to user, status code '{make_phone_call(awe_test_url)}'")
+logging.info(f"executing a phone call to user, status code '{make_phone_call(URL_TEST)}'")
 
+
+sequential_fail_count = 0
 
 # run the test case until it hits
 while True:
@@ -70,16 +85,25 @@ while True:
     if registration_status_code == 200:
         logging.info(f"the tool received a 200 from the website, registration is likely live")
         logging.info(f"executing a text message to user, status code '{send_text_message(awe_reg_message)}'")
-        logging.info(f"executing a phone call to user, status code '{make_phone_call(awe_reg_url)}'")
+        logging.info(f"executing a phone call to user, status code '{make_phone_call(URL_REG)}'")
 
         # we're done, hope you got a seat
-        break
+        logging.info(f"we're done, hope you got a seat... holding indefinitely to prevent reboot'")
+        time.sleep(60*60*24*365) # 1 year
 
     # something unexpected happened, check the tool, will continue executing just in case
     elif registration_status_code != 404:
-        logging.warning(f"the tool received an unexpected status code {registration_status_code}")
-        logging.warning(f"executing a text message to user, status code '{send_text_message(awe_warning_url)}'")
-        logging.warning(f"executing a phone call to user, status code '{make_phone_call(awe_warning_url)}'")
+        sequential_fail_count += 1
+
+        if sequential_fail_count > 5:
+            logging.warning(f"the tool received an unexpected status code {registration_status_code}")
+            logging.warning(f"executing a text message to user, status code '{send_text_message(awe_warning_message)}'")
+            logging.warning(f"executing a phone call to user, status code '{make_phone_call(URL_WARNING)}'")
+            logging.warning(f"this is the {sequential_fail_count} sequential non--404 received, something bad is happening")
+
+    # normal 404 recieved, reset failure counter
+    else: sequential_fail_count = 0
+
 
     # wait to re-test status
-    time.sleep(DELAY)
+    time.sleep(INTERVAL_SEC)
